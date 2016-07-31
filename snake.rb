006255a8cb1536c4dec6184fb2f1ef97016bc1f3
Sebastian Doerner <sebastian@sebastian-doerner.de>
2016-07-31 17:59:32 -0400

    Mostly complete Snake class

--- snake.rb
+++ snake.rb
@@ -2,6 +2,10 @@ class Point < Struct.new(:x, :y)
 	def +(other)
 		Point.new(x + other.x, y + other.y)
 	end
+
+	def to_s
+		"(#{x}, #{y})"
+	end
 end
 
 class Snake
@@ -11,8 +15,10 @@ class Snake
 	UP = Point.new(0,-1)
 	DOWN = Point.new(0,1)
 
+	attr_writer :direction
 
-	def initialize(shape = [Point.new(0,0), Point.new(1,0)])
+
+	def initialize(shape = [Point.new(0,0)])
       @shape = shape
       @direction = RIGHT
     end
@@ -30,10 +36,29 @@ class Snake
 		end
 	end
 
+    #moves the snake to the next field, not changing its size
+	def move
+		@shape << self.next
+		@shape = @shape.drop(1)
+	end
+
+	#grows the snake by advancing to the next field but not cutting its tail
+	def grow
+		@shape << self.next
+	end
+
+	def to_s
+		"Snake: [#{@shape.join(", ")}]"
+	end
 end
 
 if __FILE__ == $0
 	s = Snake.new
-	puts "#{s.next}"
-	puts "The length is #{s.length}"
+	s.move
+	puts s
+	s.grow
+	puts s
+	s.direction = Snake::DOWN
+	3.times { s.move }
+	puts s
 end
