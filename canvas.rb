00bde5ee53f763555f4a3b0dd481dedcbc5fec62
Sebastian Doerner <sebastian@sebastian-doerner.de>
2016-07-31 22:18:24 -0400

    Move canvas into a separate file

---
+++ canvas.rb
@@ -0,0 +1,23 @@
+class Canvas < Qt::Widget
+
+	def initialize(width, height, painters)
+		super()
+		@size = Qt::Size.new(width, height)
+		@painters = painters
+	end
+
+
+	def minimumSizeHint()
+	  return @size
+	end
+
+	def sizeHint()
+	  return @size
+	end
+
+	def paintEvent(event)
+		painter = Qt::Painter.new(self)
+		@painters.each { |p| p.paint(painter)}
+	  painter.end
+	end
+end
--- main.rb
+++ main.rb
@@ -1,29 +1,6 @@
 require_relative 'board'
+require_relative 'canvas'
 require 'Qt4'
-
-class Canvas < Qt::Widget
-
-	def initialize(width, height, painters)
-		super()
-		@size = Qt::Size.new(width, height)
-		@painters = painters
-	end
-
-
-	def minimumSizeHint()
-	  return @size
-	end
-
-	def sizeHint()
-	  return @size
-	end
-
-	def paintEvent(event)
-		painter = Qt::Painter.new(self)
-		@painters.each { |p| p.paint(painter)}
-	  painter.end
-	end
-end
 
 
 Qt::Application.new(ARGV) do
